2d61cbe3970fbe52b27a4ac011cfb52c0900ff70
cyanlink <tgvita@outlook.com>
2022-03-18 17:45:48 +0800

    进度检测

--- lib/my_video_crop_track/custom_scroll_track.dart
+++ lib/my_video_crop_track/custom_scroll_track.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:video_crop_track/my_video_crop_track/my_crop_clip.dart';
 import 'package:video_crop_track/no_fling_scroll_physics.dart';
@@ -13,41 +14,68 @@ class CustomScrollTrack extends StatefulWidget {
 class _CustomScrollTrackState extends State<CustomScrollTrack> {
   final _controller = ScrollController();
 
+  double progressPercentage = 0;
+
   @override
   void initState() {
     super.initState();
   }
 
+
   static int childCount = 10;
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    setupProgressListener();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        height: 100,
-        child: ChangeNotifierProvider<ScrollController>.value(
-          value: _controller,
-          child: CustomScrollView(
-              scrollDirection: Axis.horizontal,
-              physics: NoFlingScrollPhysics(parent: ClampingScrollPhysics()),
-              controller: _controller,
-              slivers: [
-                SliverPadding(
-                  padding: EdgeInsets.symmetric(horizontal: 100.0),
-                  sliver: SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      return MyCropClip(
-                        clipIndex: index,
-                        showTrailingIcon: index != childCount - 1,
-                      );
-                    },
-                    childCount: childCount,
-                  )),
-                )
-              ]),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 100,
+          child: ChangeNotifierProvider<ScrollController>.value(
+            value: _controller,
+            child: CustomScrollView(
+                scrollDirection: Axis.horizontal,
+                physics: NoFlingScrollPhysics(parent: ClampingScrollPhysics()),
+                controller: _controller,
+                slivers: [
+                  SliverPadding(
+                    padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 2),
+                    sliver: SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return MyCropClip(
+                          clipIndex: index,
+                          showTrailingIcon: index != childCount - 1,
+                        );
+                      },
+                      childCount: childCount,
+                    )),
+                  )
+                ]),
+          ),
         ),
-      ),
+        Padding(padding: EdgeInsets.all(20),child: Text("Progress: $progressPercentage%"),)
+      ]
     );
+  }
+
+  setupProgressListener() {
+    _controller.addListener(() {
+      final wholeLength = _controller.position.maxScrollExtent;
+
+      //因为在两边增加了padding，最大可滚动范围就是timeline widget全部的长度
+      final timelineLength = wholeLength;
+      final currentProgress = _controller.offset;
+
+      setState(() {
+        progressPercentage = currentProgress / timelineLength * 100;
+      });
+    });
   }
 }
--- lib/my_video_crop_track/poc.dart
+++ lib/my_video_crop_track/poc.dart
@@ -16,38 +16,17 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: CustomScrollTrack(),
+        body: GestureDetector(
+          onTap: (){
+
+          },
+          child: SizedBox.expand(
+            child: Center(
+              child: CustomScrollTrack(),
+            ),
+          ),
         ),
       ),
     );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(children: [
-      //Stack的布局逻辑是，将自身大小定为容纳所有非Positioned子组件的大小。
-      Positioned(
-        //可以通过此处的负的Offset表达startTime，同时在UI上剪裁掉被剪掉的部分
-          left: -10,
-          top: 0,
-          //SingleChildScrollView，缩略图整个一长条
-          child: Container(width: 100, height: 50, color: Colors.orange)),
-      //剪辑框
-      Container(
-        //width表示剪辑duration，加上上面offset，以此能算出endTime
-          width: 50,
-          height: 50,
-          color: Colors.transparent,
-          foregroundDecoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.white,
-              width: 2,
-            ),
-          )
-      )
-    ]);
   }
 }
